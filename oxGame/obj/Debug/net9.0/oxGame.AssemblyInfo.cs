eddd6c5645d1df0d2e6f5a765626d989e8814b6f
Dudley Mashava <seandmashava@gmail.com>
2025-04-04 15:29:39 +0200

    random secondary commit

--- oxGame/obj/Debug/net9.0/oxGame.AssemblyInfo.cs
+++ oxGame/obj/Debug/net9.0/oxGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("oxGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a0d319ac09b59adaa530e5d638f5fec2f019652")]
 [assembly: System.Reflection.AssemblyProductAttribute("oxGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("oxGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
